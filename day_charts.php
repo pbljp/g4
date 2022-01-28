5f988b08dccc492d49b1e08cab828a5c9d31f750
g198sato <g520198h@mails.cc.ehime-u.ac.jp>
2022-01-28 18:09:27 +0900

    目標作業時間と合計作業時間との比較機能を追加

--- day_charts.php
+++ day_charts.php
@@ -47,8 +47,25 @@
     $data_json = json_encode($data);
     $motivation_json = json_encode($motivation_array);
     $comment_json = json_encode($comment_array);
-    
     $stmt->close();
+
+    //合計目標時間と一日の合計作業時間の取得
+    $sql2 = "SELECT users.goal_minutes, SUM(work.working_minutes) FROM users JOIN work ON users.user_id=work.user_id WHERE users.user_id=? AND YEAR(start_time)=? AND MONTH(start_time)=? AND DAY(start_time)=? AND work.is_deleted=b'0'";
+    if($stmt2 = $mysqli->prepare($sql2)){
+        $stmt2->bind_param("siii", $user_id, $year, $month, $day);
+        $stmt2->execute();
+        $stmt2->bind_result($goal_minutes, $sum_working_minutes);
+        $stmt2->fetch();
+    }
+    $stmt2->close();
+
+    if(!isset($goal_minutes)) {
+        $goal_minutes = 1441;
+    }
+    if(!isset($sum_working_minutes)){
+        $sum_working_minutes = 0;
+    }
+
     $mysqli->close();
 ?>
 
@@ -69,7 +86,9 @@
             const motivation_array = <?php echo $motivation_json;?>;
             const comment_array = <?php echo $comment_json;?>;
 
-            console.log(data_json);
+            const goal_minutes = <?php echo $goal_minutes;?>;
+            //作業していない日の合計作業時間は0とする
+            const sum_working_minutes = <?php echo $sum_working_minutes;?>;
         </script>
 
         <link href="day_charts.css" rel="stylesheet">
@@ -79,17 +98,38 @@
         <script src="https://code.highcharts.com/modules/exporting.js"></script>
         <script src="https://code.highcharts.com/modules/accessibility.js"></script>
         <script src="day_charts.js"></script>
+
     </head>
 
-    <body>
+    <body id="hanabi">
         <?php require("header.php");?>
-        <main>
-            <div id="container"></div>
-
-            <form action="" method="POST">
-                表示する日付：<input type="date" name="date" value="<?php echo $date->format('Y-m-d');?>">&emsp;
-                <input type="submit" value="グラフ更新"><br>
-            </form>
+        <main style="width: 85%; margin-right: auto; margin-left: auto;">
+            <div id="chart" style="text-align: center; background-color: white; border: solid; padding: 20px 5px;">
+                <div id="container"></div>
+                <form action="" method="POST">
+                    表示する日付：<input type="date" name="date" value="<?php echo $date->format('Y-m-d');?>">&emsp;
+                    <input type="submit" value="グラフ更新"><br>
+                </form>
+            </div>
+            <span id="" style="text-align: right; background-color: white;">
+                目標作業時間：<?php echo $goal_minutes;?>分<br>
+                この日の作業時間：<?php echo $sum_working_minutes;?>分<br>
+                <b id="goal">目標まであと：<?php echo ($goal_minutes-$sum_working_minutes);?>分です</b>
+            </span>
         </main>
+
+        <script>
+            function launchHanabi(){
+                var pObj = document.getElementById("hanabi");
+                pObj.style.backgroundImage = 'url("hanabi.gif")';
+                pObj.style.backgroundRepeat = "no-repeat";
+                pObj.style.backgroundSize = "cover";
+            }
+            var goalText = document.getElementById("goal");
+            if(sum_working_minutes >= goal_minutes){
+                goalText.innerHTML = "目標達成<br>";
+                window.onload = launchHanabi();
+            }
+        </script>
     </body>
 </html>
