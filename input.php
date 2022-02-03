ba1477ff3375c04832199c1cd75f462a947fedf7
g408mats <87056722+g408mats@users.noreply.github.com>
2022-02-04 03:41:58 +0900

    Add files via upload

---
+++ input.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+require('library.php');
+if (isset($_SESSION['user_id']) ) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    header('Location: login.php');
+    exit();
+}
+
+//ジャンル取得
+$db = dbconnect();
+$stmt = $db->prepare('SELECT type_number, type_name FROM types WHERE user_id=?');
+if (!$stmt) {
+  die($db->error);
+}
+$stmt->bind_param('s', $user_id);
+$success = $stmt->execute();
+    
+$stmt->bind_result($type_number, $type_name);
+$i=1;
+while ($stmt->fetch()){
+  $genre_nuber[$i] = $type_number;
+  $genre[$i] = $type_name;
+  echo $genre[$i];
+  $i++;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>入力フォーム</title>
+<link rel="stylesheet" href="style.css">
+<style>
+  form {
+    width: 400px;
+    border: solid 1px black;
+    padding: 10px;
+    margin: 30px auto;
+}
+</style>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link href="header.css" rel="stylesheet">
+</head>
+
+<body>
+<?php require("header.php") ?>
+<div id="whole">
+  <div id="head">
+    <h1>項目追加</h1>
+  </div>
+  <button class="btn" onclick="location.href=''">編集</button>
+  <button class="btn" onclick="location.href=''">作業時間を見る</button>
+ <div id="content">
+   <form action="input_do.php" method="post">
+     <div class="box1">
+       <label class="box2">ジャンル</label>
+       <select name="genre">
+         <option value="<?php echo $genre[1]; ?>"><?php echo $genre[1]; ?></option>
+         <option value="<?php echo $genre[2]; ?>"><?php echo $genre[2]; ?></option>
+         <option value="<?php echo $genre[3]; ?>"><?php echo $genre[3]; ?></option>
+        </select>
+        <label class="box2">年月日</label>
+        <input type="date" name="date">
+        <label class="box2">作業時間</label>
+        <input type="time" name="times">～<input type="time" name="timee">
+        <label class="box2">モチベーション評価</label>
+        <div style="text-align: left;">
+          <input type="radio" name="motivation" value=5 id="moti5"><label for="moti5">集中して取り組めた</label><br>
+          <input type="radio" name="motivation" value=4 id="moti4"><label for="moti4">そこそこ集中できた</label><br>
+          <input type="radio" name="motivation" value=3 id="moti3"><label for="moti3">普通</label><br>
+          <input type="radio" name="motivation" value=2 id="moti2"><label for="moti2">あまり集中できなかった</label><br>
+          <input type="radio" name="motivation" value=1 id="moti1"><label for="moti1">全然集中できなかった</label>
+        </div>
+          <label>コメント</label>
+        <textarea name="comment" cols="50" rows="10" placeholeder="コメントを入力してください"></textarea>
+        <!-- <input type="file" name="" accept="image/jpeg, image/png, image/gif" multipe> -->
+      </div>
+      <p class="right">
+        <button class="btn" type="reset">クリア</button>
+        <button class="btn" type="submit">登録</button>
+        <!-- <input type="reset" value="クリア">
+          <input type="submit" value="登録"> -->
+        </p>
+      </form>
+    </div>
+</div>
+  </body>
+</html>
--- input_do.php
+++ input_do.php
@@ -1,21 +1,28 @@
 <?php
 session_start();
 require('library.php');
+if (isset($_SESSION['user_id']) ) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    header('Location: login.php');
+    exit();
+}
 
 //仮ID
-$user_id = 'user_example2';
+// $user_id = 'user_example2';
 
 $type_name = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_SPECIAL_CHARS);
 $date = $_POST['date'];
 $times = $date . ' ' . $_POST['times'];
 $timee = $date . ' ' . $_POST['timee'];
+$motivation = $_POST['motivation'];
 $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_SPECIAL_CHARS);
 
-echo $type_name . '<br>';
-echo $date . '<br>';
-echo $times . '<br>';
-echo $timee . '<br>';
-echo $comment . '<br>';
+// echo $type_name . '<br>';
+// echo $date . '<br>';
+// echo $times . '<br>';
+// echo $timee . '<br>';
+// echo $comment . '<br>';
 
 
 //type_numberの取得、オールでいいのか？
@@ -29,32 +36,84 @@ $success1 = $x->execute();
 
 $x->bind_result($type_number);
 $x->fetch();
-echo 'type_number=' . $type_number . '<br>';
+// echo 'type_number=' . $type_number . '<br>';
 //-----------------------------------
 
+//時間の計算
 $start = strtotime($times);
 $end = strtotime($timee);
 $sum = ($end - $start) / 60;
-echo $sum . '分 <br>';
+// echo $sum . '分 <br>';
+//-------------------------
+
+//時間の重複確認 *データ数が増えるにつれ時間が長くなるかもー＞月ごとの指定を行う予定
+function isTimeDuplication($startTime1, $endTime1, $startTime2, $endTime2) {
+    return ($startTime1 < $endTime2 && $startTime2 < $endTime1);
+}
+
+$db = dbconnect();
+$stmt1 = $db->prepare('select start_time, finish_time  from work where user_id=? order by work_id desc');
+if (!$stmt1) {
+    die($db->error);
+}
+
+$stmt1->bind_param('s', $user_id);
+$success = $stmt1->execute();
+if(!$success) {
+    die($db->error);
+}
+
+$stmt1->bind_result($sTime, $eTime);
+while ($stmt1->fetch()) {
+    $startTime = strtotime($sTime);
+    $endTime = strtotime($eTime);
+
+    if (isTimeDuplication($startTime, $endTime, $start, $end) == 1){
+        echo '時間が重複しています <br>';
+        echo '<a href="homepage.php">ホームに戻る</a><br>';
+        echo '<a href="input.html">&laquo;&nbsp;再入力</a>';
+        exit;
+    }
+}
 
 $picture_name = '';
 //-----------------------------------
 
-$db = new mysqli('localhost:8889', 'root', 'root', 'mydb_new');
-
-$stmt = $db->prepare('insert into work(user_id, type_number, start_time, finish_time, working_minutes, comment, picture_name)  values(?, ?, ?, ?, ?, ?, ?)');
+$db = dbconnect();
+$stmt = $db->prepare('insert into work(user_id, type_number, start_time, finish_time, working_minutes, comment, picture_name, motivation)  values(?, ?, ?, ?, ?, ?, ?, ?)');
 if(!$stmt):
     die($db->error);
 endif;
-$stmt->bind_param('sississ', $user_id , $type_number, $times, $timee, $sum, $comment, $picture_name);
+$stmt->bind_param('sississi', $user_id , $type_number, $times, $timee, $sum, $comment, $picture_name, $motivation);
 $ret = $stmt->execute();
-
-if($ret):
-    echo '登録されました  <br>';
-    echo '<a href="">ホームに戻る</a><br>';
-    echo '<a href="input.html">&laquo;&nbsp;続けて入力する</a>';
-else:
-    echo $db->error;
-endif;
-
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>input_do.php</title>
+</head>
+<body>
+
+    <div id="whole">
+        <div id="head">
+            <h1>項目追加</h1>
+        </div>
+        <div id="content">
+        <?php
+        if($ret):
+            echo '登録されました  <br>';
+            echo '<a href="homepage.php">ホームに戻る</a><br>';
+            echo '<a href="input.php">&laquo;&nbsp;続けて入力する</a>';
+        else:
+            echo $db->error;
+        endif;
+        ?>
+        </div>
+    </div>
+</body>
+</html>
