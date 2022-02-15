5f0b1f4c877b6bb8c34aeef789f0b60ec9324c45
g408mats <87056722+g408mats@users.noreply.github.com>
2022-02-15 09:08:38 +0900

    Update input.php

--- input.php
+++ input.php
@@ -4,7 +4,7 @@ require('library.php');
 if (isset($_SESSION['user_id']) ) {
     $user_id = $_SESSION['user_id'];
 } else {
-    header('Location: login.php');
+    header('Location: .php');
     exit();
 }
 
